ad3f0920f9790e2147a9901b652f98fbcd101aab
Angel Rosado <angel.rosado1@upr.edu>
2020-02-06 00:05:19 -0600

    add scripts to trash

---
+++ trash/njet_norm.py
@@ -0,0 +1,116 @@
+# njet normalization after applygin shape 
+
+import sys
+import os
+import numpy as np
+import ROOT
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+
+#--------------------------------------------------------------------------------------------------------------
+# Open root file
+#--------------------------------------------------------------------------------------------------------------
+
+year = sys.argv[1]
+
+root_file = ROOT.TFile.Open("/uscms/home/arosado/nobackup/YOURWORKINGAREA/CMSSW_10_2_9/src/ZInvisible/Tools/condor/eighth_run/" + year + "/result.root", 'read')
+in_root = 'nJets_drLeptonCleaned_jetpt20'
+
+particles  =  ['Electron', 'Muon']
+regions    =  ['HighDM', 'LowDM']
+metcuts    =  [""]
+
+#--------------------------------------------------------------------------------------------------------------
+# Load Histograms
+#--------------------------------------------------------------------------------------------------------------
+
+histos =  {}
+
+for particle in particles:
+    histos[particle] = {}
+    for region in regions:
+        histos[particle][region] = {}
+        for metcut in metcuts: 
+            histos[particle][region][metcut] = {}
+
+            # Histograms names
+
+            err = "{}".format("" if not metcut else "_")
+            prefix = 'DataMC_' + particle + '_' + region + '_njetWeight_' + metcut + err + 'nj_jetpt20_' + year + 'nJets_drLeptonCleaned_jetpt20nJets_drLeptonCleaned_jetpt20'
+            Data  = prefix + 'Datadata'
+            DY    = prefix + 'DYstack'
+            Sint  = prefix + 'Single tstack'
+            TTbar = prefix + 't#bar{t}stack'
+            Rare  = prefix + 'Rarestack'
+
+            # Retriving histograms
+
+            histos[particle][region][metcut]['Data']  = root_file.Get(in_root + '/' + Data)
+            histos[particle][region][metcut]['DY']    = root_file.Get(in_root + '/' + DY)
+            histos[particle][region][metcut]['Sint']  = root_file.Get(in_root + '/' + Sint)
+            histos[particle][region][metcut]['TTbar'] = root_file.Get(in_root + '/' + TTbar)
+            histos[particle][region][metcut]['Rare']  = root_file.Get(in_root + '/' + Rare)
+
+            # Verify if histograms exist
+
+	    if not histos[particle][region][metcut]['Data']:
+		print(in_root + '/' + Data + " doesn't exist!")
+	    if not histos[particle][region][metcut]['DY']:
+		print(in_root + '/' + DY + " doesn't exist!")
+	    if not histos[particle][region][metcut]['Sint']:
+		print(in_root + '/' + Sint + " doesn't exist!")
+	    if not histos[particle][region][metcut]['TTbar']:
+		print(in_root + '/' + TTbar + " doesn't exist!")
+	    if not histos[particle][region][metcut]['Rare']:
+		print(in_root + '/' + Rare + " doesn't exist!")
+
+#--------------------------------------------------------------------------------------------------------------
+# Calculating Normalization
+#--------------------------------------------------------------------------------------------------------------
+
+norms = {}
+bin_1  = 0 
+bin_2 = histos['Electron']['LowDM']['']['Data'].GetNbinsX()
+
+for region in regions:
+    norms[region] = {}
+    Num = 0
+    Dem = 0
+    for particle in particles:
+        
+        # version 1 
+        # I_Data  =  histos[particle][region][""]["Data"].Integral(bin_1, bin_2)  
+        # I_DY    =  histos[particle][region][""]["DY"].Integral(bin_1, bin_2)
+        # I_Sint  =  histos[particle][region][""]["Sint"].Integral(bin_1, bin_2)
+        # I_TTbar =  histos[particle][region][""]["TTbar"].Integral(bin_1, bin_2)
+        # I_Rare  =  histos[particle][region][""]["Rare"].Integral(bin_1, bin_2)
+        # I_Numerator   =  I_Data - (I_Sint + I_TTbar + I_Rare)
+        # I_Denominator =  I_DY
+
+        h_Numerator = histos[particle][region][""]['Data'].Clone() 
+        h_Numerator.Add( histos[particle][region][""]['Sint'] , -1)  
+        h_Numerator.Add( histos[particle][region][""]['TTbar'], -1) 
+        h_Numerator.Add( histos[particle][region][""]['Rare'] , -1)  
+        h_Denominator = histos[particle][region][""]['DY'].Clone()
+        I_Numerator = h_Numerator.Integral(bin_1, bin_2) 
+        I_Denominator = h_Denominator.Integral(bin_1, bin_2)
+    
+        Num += I_Numerator
+        Dem += I_Denominator
+
+        norms[region][particle] =  I_Numerator/I_Denominator
+        
+    norms[region]["Combine"] = Num/Dem
+
+#--------------------------------------------------------------------------------------------------------------
+# print normalization factors
+#--------------------------------------------------------------------------------------------------------------
+particles.append("Combine")
+
+for particle in particles:
+    for region in regions:
+        print("\n" + particle + " " + region + ": " + str(norms[region][particle]))
+ #       with open('normalizations.txt', 'a+') as sheet:
+ #           sheet.write("\hline" + region + " & " + particle + " & " + year + " & " + str(norms[region][particle]) + " \n") 
+
---
+++ trash/njet_shape.py
@@ -0,0 +1,217 @@
+# njets all
+
+import sys
+import os
+import numpy as np
+import ROOT
+import json
+
+# make sure ROOT.TFile.Open(fileURL) does not seg fault when $ is in sys.argv (e.g. $ passed in as argument)
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+# make plots faster without displaying them
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+
+# Get root file explicitly
+
+location = sys.argv[1]
+year  = sys.argv[2]
+
+root_file = ROOT.TFile.Open(location)
+in_root = 'nJets_drLeptonCleaned_jetpt30'
+f = ROOT.TFile("shapes_njets_" + year + ".root", 'recreate')
+
+# Get histograms
+
+particles  =  ['Electron', 'Muon', 'Combine']
+regions    =  ['HighDM', 'LowDM']
+metcuts    =  ['Loose']
+
+norms  =  {}
+histos =  {}
+shapes =  {}
+
+for particle in particles:
+
+    histos[particle] = {}
+    shapes[particle] = {}
+
+    for region in regions:
+
+        histos[particle][region] = {}
+        shapes[particle][region] = {}
+
+        for metcut in metcuts: 
+            shapes[particle][region][metcut] = {
+                'Numerator'    :  0,
+                'Denominator'  :  0,
+                'Shape'        :  0
+                }        
+
+            if particle == 'Combine':
+                continue
+            if metcut == 'MidHT' and region == 'LowDM':
+                continue
+
+            prefix = 'DataMC_' + particle + '_' + region + '_' + metcut
+            err = "{}".format("" if not metcut else "_")
+
+            histos[particle][region][metcut] = {
+		'Data'  :  prefix + err + 'nj_jetpt30_' + year + 'nJets_drLeptonCleaned_jetpt30nJets_drLeptonCleaned_jetpt30' + 'Datadata',
+		'DY'    :  prefix + err + 'nj_jetpt30_' + year + 'nJets_drLeptonCleaned_jetpt30nJets_drLeptonCleaned_jetpt30' + 'DYstack',
+		'Sint'  :  prefix + err + 'nj_jetpt30_' + year + 'nJets_drLeptonCleaned_jetpt30nJets_drLeptonCleaned_jetpt30' + 'Single tstack',
+		'TTbar' :  prefix + err + 'nj_jetpt30_' + year + 'nJets_drLeptonCleaned_jetpt30nJets_drLeptonCleaned_jetpt30' + 't#bar{t}stack',
+		'Rare'  :  prefix + err + 'nj_jetpt30_' + year + 'nJets_drLeptonCleaned_jetpt30nJets_drLeptonCleaned_jetpt30' + 'Rarestack'
+		}
+
+	    print('We are now in: ' + particle + ' ' + region + ' ' + metcut) #debbuging
+
+# Retriving histograms
+
+	    h_Data   =  root_file.Get(in_root + '/' + histos[particle][region][metcut]['Data'])
+	    h_DY     =  root_file.Get(in_root + '/' + histos[particle][region][metcut]['DY'])
+	    h_Sint   =  root_file.Get(in_root + '/' + histos[particle][region][metcut]['Sint'])
+	    h_TTbar  =  root_file.Get(in_root + '/' + histos[particle][region][metcut]['TTbar'])
+	    h_Rare   =  root_file.Get(in_root + '/' + histos[particle][region][metcut]['Rare'])
+
+            # print str(h_Data.GetNbinsX()) + ' bins when retriving histograms' #bins counter
+
+# Verify if histograms exist
+
+	    if not h_Data:
+		print(in_root + '/' + histos[particle][region][metcut]['Data'] + " doesn't exist!")
+	    if not h_DY:
+		print(in_root + '/' + histos[particle][region][metcut]['DY'] + " doesn't exist!")
+	    if not h_Sint:
+		print(in_root + '/' + histos[particle][region][metcut]['Sint'] + " doesn't exist!")
+	    if not h_TTbar:
+		print(in_root + '/' + histos[particle][region][metcut]['TTbar'] + " doesn't exist!")
+	    if not h_Rare:
+		print(in_root + '/' + histos[particle][region][metcut]['Rare'] + " doesn't exist!")
+
+# Rebining
+
+     #       h_Data.Rebin(2)
+     #       h_DY.Rebin(2)
+     #       h_Sint.Rebin(2)
+     #       h_TTbar.Rebin(2)
+     #       h_Rare.Rebin(2)
+
+            bin_1  = 0 
+            bin_2 = h_Data.GetNbinsX()
+
+            # print str(h_Data.GetNbinsX()) + ' bins after rebinning ' #bins counter
+
+# Shape factor
+
+            h_Shape = h_Data.Clone()
+            h_Shape.SetName("njets_shape_" + year + "_" + particle + '_' + region + err + metcut)
+            h_Shape.Add(h_TTbar, -1)
+            h_Shape.Add(h_Rare, -1)
+            h_Shape.Add(h_Sint, -1)
+
+            shapes[particle][region][metcut]['Numerator']   =  h_Shape.Clone()
+            shapes[particle][region][metcut]['Denominator'] =  h_DY.Clone()
+
+            I_Numerator = h_Shape.Integral(bin_1, bin_2)
+
+	    h_Shape.Divide(h_DY)
+
+            shapes[particle][region][metcut]['Shape']  =  h_Shape.Clone()
+
+# Normalization factor
+
+            h_Denominator = h_DY.Clone()
+            h_Denominator.Multiply(h_Shape)
+            I_Denominator = h_Denominator.Integral(bin_1, bin_2)
+
+            print "In " + particle + " " + region + " " + metcut
+            print "I_Numerator = " + str(I_Numerator)
+            print "I_Denominator = " + str(I_Denominator)
+
+# Canvas
+
+            h_Shape.GetXaxis().SetRangeUser(2,10)
+            h_Shape.GetYaxis().SetRangeUser(0,3)
+
+# Corroboration sheet
+
+            if False:
+	        table = {}
+	        for binn in range(0,40):
+	            table[binn] = {
+		        'data'  :  h_Data.GetBinContent(binn),
+		        'sine'  :  h_Sint.GetBinContent(binn),
+		        'ttbar' :  h_TTbar.GetBinContent(binn),
+		        'rare'  :  h_Rare.GetBinContent(binn),
+		        'dy'    :  h_DY.GetBinContent(binn),
+		        'shape' :  h_Shape.GetBinContent(binn)
+	            }
+
+                print str(h_Rare.GetNbinsX()) + ' bins after rebinning'
+
+	        with open('corroboration_sheet_' + particle + '_' + region + '_' + metcut + '.txt', 'w') as sheet:
+		    sheet.write("bins: 1 to 40\n\n")
+		    for h in ['data', 'sine', 'ttbar', 'rare', 'dy', 'shape']:
+		        for n in range(1,40):
+		            sheet.write(str(table[n][h]) + '  ')
+		        sheet.write('\n\n')
+
+# Create a canvas
+
+	    canvas = ROOT.TCanvas("c", "c", 800, 800)
+
+# Draw legend
+
+	    legend = ROOT.TLegend(0.5, 0.7, 0.9, 0.9)
+	    legend.AddEntry(h_Shape, 'Shape', '1')
+	    legend.AddEntry(h_Data, 'Data', '1')
+	    legend.Draw()
+
+# Draw histogram
+
+	    h_Shape.Draw('error')
+	    canvas.Update()
+
+ #Save new Histogram as png and root
+
+	    if not metcut:
+                file_name = prefix + 'nj_' + year + 'metWithLL_Shape.png'
+            else:
+                file_name = prefix + '_nj_' + year + 'metWithLL_Shape.png'
+
+	    canvas.SaveAs(file_name)
+
+            h_Shape.Write()
+
+# End of loop--------------------------------------------------------------------------------
+
+# Combine shape factors
+
+
+for region in regions:
+    for metcut in metcuts:
+        if metcut == 'MidHT' and region == 'LowDM':
+            continue
+    
+	print 'We are now in: ' + region + ' ' + metcut  #debbuging
+
+        shapes['Combine'][region][metcut]['Numerator']  =  shapes['Electron'][region][metcut]['Numerator'].Clone()
+        shapes['Combine'][region][metcut]['Numerator'].Add(shapes['Muon'][region][metcut]['Numerator'])
+
+        shapes['Combine'][region][metcut]['Denominator']  =  shapes['Electron'][region][metcut]['Denominator'].Clone()
+        shapes['Combine'][region][metcut]['Denominator'].Add(shapes['Muon'][region][metcut]['Denominator'])
+
+        shapes['Combine'][region][metcut]['Shape']  =  shapes['Combine'][region][metcut]['Numerator'].Clone()
+        shapes['Combine'][region][metcut]['Shape']  =  shapes['Combine'][region][metcut]['Shape'].Divide(shapes['Combine'][region][metcut]['Denominator'])
+
+# Save Normalization and shape factors
+
+ #for region in regions:
+ #    for particle in particles:
+ #        for metcut in metcuts:
+ #            if metcut == 'MidHT' and region == 'LowDM':
+ #                continue
+ #            with open('normalizations.txt', 'a+') as sheet:
+ #                sheet.write(year + " " + region + " " + particle + " " + metcut + " : " + str(norms[particle][region][metcut]['R_Z']) + "\n")
+
+f.Close()

828bdfb378db3d30e20ec5fc32f7506b247611da
Angel Rosado <angel.rosado1@upr.edu>
2020-03-02 12:14:31 -0600

    add ForStatSyst in LoadHistograms.py (is not functional yet)

---
+++ modules/LowDMHighMET_merger.py
@@ -0,0 +1,25 @@
+# merge LowDM and LowDMHighMET bins 
+
+import ROOT
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+
+def LowDMMergerValidationBins(lowMET, highMET):
+    """Merge LowDM with LowDMHighMET in the validation bins"""
+
+    merged = ROOT.TH1F( "nValidationBinLowDM_jetpt30", "nValidationBinLowDM_jetpt30", 19, 0, 19 )
+    
+    for k in range(1,20):
+        if k >= 16: 
+            a   =  highMET.GetBinContent(k-15)
+            da  =  highMET.GetBinError(k-15) 
+        else:
+            a   =  lowMET.GetBinContent(k)
+            da  =  lowMET.GetBinError(k) 
+    
+        merged.SetBinContent( k, a ) 
+        merged.SetBinError( k, da )
+    
+    return merged
+
